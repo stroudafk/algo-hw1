e6d86b1846a0f8161f8bf2fd66769b12b1a7e1c2
SJ Stroud <70240650+stroudafk@users.noreply.github.com>
2022-01-27 08:04:58 +0000

    uncommented test cases

--- main.cpp
+++ main.cpp
@@ -6,40 +6,40 @@
 
 int main() {
 
-  // std::cout << "Q1" << std::endl;
-  // std::cout << findMissingNumber({0, 1, 2, 4, 5}) << std::endl;  // should output 3
-  // std::cout << findMissingNumber({5, 0, 4, 3, 1}) << std::endl;  // should output 2
-  // std::cout << findMissingNumber({}) << std::endl;  // should output 0
-  // std::cout << findMissingNumber({9, 3, 5, 1, 4, 8, 2, 10, 0, 6}) << std::endl;  // should output 7
+  std::cout << "Q1" << std::endl;
+  std::cout << findMissingNumber({0, 1, 2, 4, 5}) << std::endl;  // should output 3
+  std::cout << findMissingNumber({5, 0, 4, 3, 1}) << std::endl;  // should output 2
+  std::cout << findMissingNumber({}) << std::endl;  // should output 0
+  std::cout << findMissingNumber({9, 3, 5, 1, 4, 8, 2, 10, 0, 6}) << std::endl;  // should output 7
 
-  // std::cout << "Q2" << std::endl;
-  // std::cout << findMaxOfVector({1, 3, 4, 5, 2}) << std::endl;  // should output 5
-  // std::cout << findMaxOfVector({-1, -3, -4, -5, -2}) << std::endl;  // should output -1
-  // bool emptyCaseCorrect = false;
-  // try {
-  //   std::cout << findMaxOfVector({}) << std::endl;  // should throw
-  // } catch (const std::invalid_argument& ia) {
-  //   emptyCaseCorrect = true;
-  // }
-  // if (emptyCaseCorrect) {
-  //   std::cout << "findMaxOfVector appears correct for the empty case." << std::endl;
-  // } else {
-  //   std::cout << "findMaxOfVector appears incorrect for the empty case."
-  //             << std::endl;
-  // }
+  std::cout << "Q2" << std::endl;
+  std::cout << findMaxOfVector({1, 3, 4, 5, 2}) << std::endl;  // should output 5
+  std::cout << findMaxOfVector({-1, -3, -4, -5, -2}) << std::endl;  // should output -1
+  bool emptyCaseCorrect = false;
+  try {
+    std::cout << findMaxOfVector({}) << std::endl;  // should throw
+  } catch (const std::invalid_argument& ia) {
+    emptyCaseCorrect = true;
+  }
+  if (emptyCaseCorrect) {
+    std::cout << "findMaxOfVector appears correct for the empty case." << std::endl;
+  } else {
+    std::cout << "findMaxOfVector appears incorrect for the empty case."
+              << std::endl;
+  }
 
-//   std::cout << "Q3" << std::endl;
-//   std::vector<int> q3Input1{0, 2, 3, 4, 5};
-//   std::pair<int, int> q3Out1 = findTwoSumPair(q3Input1, 6);
-//   std::pair<int, int> q3Out2 = findTwoSumPair(q3Input1, 10);
-//   std::pair<int, int> q3Out3 = findTwoSumPair(q3Input1, 5);
+  std::cout << "Q3" << std::endl;
+  std::vector<int> q3Input1{0, 2, 3, 4, 5};
+  std::pair<int, int> q3Out1 = findTwoSumPair(q3Input1, 6);
+  std::pair<int, int> q3Out2 = findTwoSumPair(q3Input1, 10);
+  std::pair<int, int> q3Out3 = findTwoSumPair(q3Input1, 5);
 
-//   std::cout << q3Out1.first << " " << q3Out1.second
-//             << std::endl;  // should output 1 3
-//   std::cout << q3Out2.first << " " << q3Out2.second
-//             << std::endl;  // should output -1 -1
-// std::cout << q3Out3.first << " " << q3Out3.second
-// << std::endl;  // should output 1 2
+  std::cout << q3Out1.first << " " << q3Out1.second
+            << std::endl;  // should output 1 3
+  std::cout << q3Out2.first << " " << q3Out2.second
+            << std::endl;  // should output -1 -1
+std::cout << q3Out3.first << " " << q3Out3.second
+<< std::endl;  // should output 1 2
 
   std::cout << "Q4" << std::endl;
   Node<int>* q4bInput1 = makeList(123);
